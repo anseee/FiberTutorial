7a918e6b7c0554c19416bc3728df6644684e0591
anseee <parksungwon1113@gmail.com>
2022-03-20 00:36:10 +0900

    [*][+] add routes | add static html

--- server.go
+++ server.go
@@ -5,8 +5,32 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	app.Static("/", "./public")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/:value", func(c *fiber.Ctx) error {
+		return c.SendString("value: " + c.Params("value"))
+		// => Get request with value: hello world
+	})
+
+	// GET http://localhost:3000/john
+
+	app.Get("/:name?", func(c *fiber.Ctx) error {
+		if c.Params("name") != "" {
+			return c.SendString("Hello " + c.Params("name"))
+			// => Hello john
+		}
+		return c.SendString("Where is john?")
+	})
+
+	// GET http://localhost:3000/api/user/john
+
+	app.Get("/api/*", func(c *fiber.Ctx) error {
+		return c.SendString("API path: " + c.Params("*"))
+		// => API path: user/john
 	})
 
 	app.Listen(":3000")
